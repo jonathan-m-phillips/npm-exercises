5192ad60ddc7da05f43299c5b984e8976a5492cd
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-08-03 15:15:20 -0500

    modules exercise

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@
 const sayHello = () => console.log('hello');
 
 sayHello();
+
+import helloWorld from './say-hello.js';
+
+helloWorld();
---
+++ src/say-hello.js
@@ -0,0 +1,7 @@
+
+
+const helloWorld = () => console.log('hello world, it is beautiful out today!');
+
+helloWorld();
+
+module.exports = helloWorld;
